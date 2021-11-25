7eef196eab567c25698ef91748d3486e06073857
RUBESH_THIRUMANI <33860839+Rubeshthirumani@users.noreply.github.com>
2021-11-25 10:23:29 +0530

    Update gaussian_blur.py

--- smooth_and_filter/gaussian_blur.py
+++ smooth_and_filter/gaussian_blur.py
@@ -2,7 +2,7 @@
 # A Gaussian kernel is a kernel with the shape of a Gaussian (normal distribution) curve.
 import cv2
 img=cv2.imread("messi.jpg")
-gaussian_blur=cv2.GaussianBlur(i,(11,11),20)
+gaussian_blur=cv2.GaussianBlur(img,(11,11),20)
 cv2.imshow("GAUSSIAN BLUR",gaussian_blur)
 cv2.waitKey(0)
 #APPLICATION:Gaussian filtering is highly effective in removing Gaussian noise from the image.
